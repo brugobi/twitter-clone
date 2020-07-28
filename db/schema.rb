88443ea6e9cb6544a7a3d83d67d2c0d6f7e446f2
brugobi <brgobbi@hotmail.com>
2020-07-28 15:56:34 -0400

    finish the app

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_28_135931) do
+ActiveRecord::Schema.define(version: 2020_07_28_163440) do
 
   create_table "tweeets", force: :cascade do |t|
     t.text "tweeet"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
